b71ac4473e8d3acd2b1b4e886f0e76850e02a2d0
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2020-03-30 22:04:34 -0700

    added error

--- matchmaker.py
+++ matchmaker.py
@@ -4,7 +4,8 @@ from datetime import datetime, timedelta
 
 
 class recipientProfile(object):
-    def __init__(self, recipient_name, recipient_email, amount_recieved, date_created, num_donations):
+    def __init__(self, recipient_name, recipient_email, amount_recieved, date_created, num_donations, user_id):
+        self._user_id = user_id
         self._amount_receieved = amount_recieved
         self._date_created = date_created
         self._recipient_name = recipient_name
@@ -12,12 +13,15 @@ class recipientProfile(object):
         self._num_donations = num_donations
 
         # setting burn rate adjusted amount recieived
-        # using a 20$ per week rate to drop rate
+        # using a 20$ per week rate to drop the amount
         # in other words, drop amount by 0.119$ every hour
         hours_since_creation = (
-            (datetime.now - date_created).total_seconds() / 60)
+            (datetime.now() - date_created).total_seconds() / 60)
         burn_rate = -0.119
-        self._burn_adj_amount_recieved = amount_recieved*burn_rate*time_since_creation
+        self._burn_adj_amount_recieved = amount_recieved*burn_rate*hours_since_creation
+
+    def get_user_id(self):
+        return self._user_id
 
     def get_name(self):
         return self._recipient_name
@@ -34,58 +38,88 @@ class recipientProfile(object):
     def get_num_donations(self):
         return self._num_donations
 
-    # reference: https://portingguide.readthedocs.io/en/latest/comparisons.html
-    def __eq__(self, other):
+    def get_compare_value(self, other):
         return (self._burn_adj_amount_recieved == other._burn_adj_amount_recieved) & (self._num_donations == other._num_donations) & (self._date_created == other._date_created)
 
+    # reference: https://portingguide.readthedocs.io/en/latest/comparisons.html
+
+    def __eq__(self, other):
+        return self.get_compare_value(self, other)
+
     def __ne__(self, other):
-        return not (self._burn_adj_amount_recieved == other._burn_adj_amount_recieved) & (self._num_donations == other._num_donations) & (self._date_created == other._date_created)
+        return not self.get_compare_value(self, other)
 
     def __lt__(self, other):
-        if (self._burn_adj_amount_recieved < other._burn_adj_amount_recieved) is True:
+        if (self._burn_adj_amount_recieved < other._burn_adj_amount_recieved) == True:
             return True
-        elif (self._num_donations < other._num_donations) is True:
+        elif (self._num_donations < other._num_donations) == True:
             return True
         # the sign is flipped here bc the older is considered "less" in this case as we want to support the individual with a greater date created but a lower balance and num donations
-        elif (self._date_created > other._date_created) is True:
+        elif (self._date_created > other._date_created) == True:
             return True
         else:
             return False
 
     def __le__(self, other):
-        if (self._burn_adj_amount_recieved <= other._burn_adj_amount_recieved) is True:
+        if (self._burn_adj_amount_recieved <= other._burn_adj_amount_recieved) == True:
             return True
-        elif (self._num_donations <= other._num_donations) is True:
+        elif (self._num_donations <= other._num_donations) == True:
             return True
         # the sign is flipped here bc the older is considered "less" in this case as we want to support the individual with a greater date created but a lower balance and num donations
-        elif (self._date_created >= other._date_created) is True:
+        elif (self._date_created >= other._date_created) == True:
             return True
         else:
             return False
 
     def __gt__(self, other):
-        if (self._burn_adj_amount_recieved > other._burn_adj_amount_recieved) is True:
+        if (self._burn_adj_amount_recieved > other._burn_adj_amount_recieved) == True:
             return True
-        elif (self._num_donations > other._num_donations) is True:
+        elif (self._num_donations > other._num_donations) == True:
             return True
         # the sign is flipped here bc the older is considered "less" in this case as we want to support the individual with a greater date created but a lower balance and num donations
-        elif (self._date_created < other._date_created) is True:
+        elif (self._date_created < other._date_created) == True:
             return True
         else:
             return False
 
     def __ge__(self, other):
-        if (self._burn_adj_amount_recieved >= other._burn_adj_amount_recieved) is True:
+        if (self._burn_adj_amount_recieved >= other._burn_adj_amount_recieved) == True:
             return True
-        elif (self._num_donations >= other._num_donations) is True:
+        elif (self._num_donations >= other._num_donations) == True:
             return True
         # the sign is flipped here bc the older is considered "less" in this case as we want to support the individual with a greater date created but a lower balance and num donations
-        elif (self._date_created <= other._date_created) is True:
+        elif (self._date_created <= other._date_created) == True:
             return True
         else:
             return False
+
+    def __repr__(self):
+        return "Name: %s | Email: %s | UID: %s" % (self._recipient_name, self._recipient_email, self._user_id)
 
 
 # use to implement priority queue and house method that will actually return the desired tuple of
 # userid and email
 class Matchmaker:
+    # this class builds the priority queue using the objects above
+
+    def __init__(self):
+        pass
+
+    # get user data from SQL table
+    # convert to recipientProfile objects and store in an array
+    # put all the objects in priority queue
+    # def get_user_data(self):
+
+    a = recipientProfile("John Smit", "john@test.com",
+                         45, datetime.date(2002, 3, 11), 4, 'ere33')
+
+    c = recipientProfile("John Smit", "john@test.com",
+                         45, datetime.date(2002, 3, 11), 4, 'ere33')
+
+    b = recipientProfile("Alex Jo", "alex@test.com",
+                         45, datetime.now(), 4, 'afdj33')
+
+    print(a.get_date_created())
+    print(c.get_date_created())
+
+    print(a < c)
--- server.py
+++ server.py
@@ -35,22 +35,25 @@ def handle_invalid_usage(error):
 @app.route('/purchase', methods=['POST'])
 def purch():
     if not has_args(request.json, ['sender_email', 'sender_address', 'city', 'state', 'zipcode', 'cardholder_name', 'card_number', 'exp_date', 'cvv']):
-        raise InvalidUsage('note all paramenters present')
+        raise InvalidUsage('not all paramenters present')
 
     # response = jsonify(error.to_dict())
     # response.status_code = error.status_code
 
     # updates the opening prices dictionary in stockFilter
     request.json['ticker']
-    status = purchase(sender_email=request.json['sender_email'], sender_address=request.json['sender_address'], city=request.json['city'], state=request.json['state'], zipcode=request.json['zipcode'], cardholder_name=request.json['cardholder_name'], card_number=request.json['card_number'], exp_date=request.json['exp_date'], cvv=request.json['cvv'])
+    status = purchase(sender_email=request.json['sender_email'], sender_address=request.json['sender_address'], city=request.json['city'], state=request.json['state'],
+                      zipcode=request.json['zipcode'], cardholder_name=request.json['cardholder_name'], card_number=request.json['card_number'], exp_date=request.json['exp_date'], cvv=request.json['cvv'])
 
     return status
 
-@app.route('/preFill', methods = ['POST'])
+
+@app.route('/preFill', methods=['POST'])
 def pref():
     if not has_args(request.json, ['dollars', 'recipient_name', 'recipient_email', 'sender_name']):
         raise InvalidUsage('note all paramenters present')
-    status = preFill(dollars=request.json['dollars'], recipient_name=request.json['recipient_name'], recipient_email=request.json['recipient_email'], sender_name=request.json['sender_name'])
+    status = preFill(dollars=request.json['dollars'], recipient_name=request.json['recipient_name'],
+                     recipient_email=request.json['recipient_email'], sender_name=request.json['sender_name'])
     return status
 
 
