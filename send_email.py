3eecbb3d1e8e8dcc08b1f3c1c61667725d70b74b
sjhangiani12 <sjhangiani12@users.noreply.github.com>
2020-04-05 00:38:11 -0700

    email up and runnin baby

--- send_email.py
+++ send_email.py
@@ -1,18 +1,26 @@
-import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+# Code from best solution in page below:
+# https://help.zoho.com/portal/community/topic/zoho-mail-servers-reject-python-smtp-module-communications
 
-message = Mail(
-    from_email='sharan@care37.org',
-    to_emails='sharan@uw.edu',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
-try:
-    sg = SendGridAPIClient('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')
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print('failed')
-    print(e)
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import formataddr
+
+# Define to/from
+sender = 'admin@care37.org'
+sender_title = "Sup Bitch"
+recipient = 'rlin20@uw.edu'
+
+# Create message
+msg = MIMEText("Did ya kno Nikhil eats ASS", 'plain', 'utf-8')
+msg['Subject'] =  Header("Sent from python", 'utf-8')
+msg['From'] = formataddr((str(Header(sender_title, 'utf-8')), sender))
+msg['To'] = recipient
+
+# Create server object with SSL option
+server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
+
+# Perform operations via server
+server.login('admin@care37.org', 'Care37Pwd')
+server.sendmail(sender, [recipient], msg.as_string())
+server.quit()
