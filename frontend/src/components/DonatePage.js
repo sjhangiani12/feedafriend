df139e1a8767b28d8a7171cf796a261faab97385
Nikhil Sharma <chesskid98@gmail.com>
2020-04-05 18:10:03 -0700

    started on donation page, UI is a pain :(

--- frontend/src/components/DonatePage.js
+++ frontend/src/components/DonatePage.js
@@ -1,38 +1,66 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import CreditCardInput from 'react-credit-card-input';
 
-function DonatePage () {
+function DonatePage() {
 
-  const [cardNumber, setCardNumber]  = useState(undefined);
-  const [expiry, setExpiry] = useState(undefined);
-  const [cvc, setCardCVC]  = useState(undefined);
+  const header = {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1",
+    justifyContent: "space-between"
+  }
 
-  const root = {
+  const bigTextFlex = {
+    flex: "1",
+  }
+
+  const bigText = {
     position: "absolute",
-    top: "25%",
-    left: "20%",
-    width: "60%"
+    width: "551px",
+    height: "162px",
+    left: "78px",
+    top: "287px",
+    fontFamily: "Abril Titling",
+    fontStyle: "normal",
+    fontWeight: "600",
+    fontSize: "72px",
+    lineHeight: "81px"
+  }
+  
+  const donationText = {
+    flex: "1",
+    position: "absolute",
+    width: "378px",
+    height: "40px",
+    left: "786px",
+    top: "315px"
+  }
+
+  const step = {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    fontSize: "18px",
+    lineHeight: "21px"
+  }
+
+  const enterDonation = {
+    fontFamily: "Abril Titling",
+    fontStyle: "normal",
+    fontWeight: "600",
+    fontSize: "36px",
+    lineHeight: "40px"
   }
 
   return (
-    <div style={root}>
-      <form>
-        <label>Name</label> &nbsp; &nbsp; &nbsp;<input type="text" />
-        <p />
-        <label>Email</label> &nbsp; &nbsp; &nbsp;<input type="email" />
-        <p />
-        <CreditCardInput
-          cardNumberInputProps={{ value: cardNumber, onChange: (event) => setCardNumber(event.target.value) }}
-          cardExpiryInputProps={{ value: expiry, onChange: (event) => setExpiry(event.target.value) }}
-          cardCVCInputProps={{ value: cvc, onChange: (event) => setCardCVC(event.target.value) }}
-          fieldClassName="input"
-        />
-        <p />
-        <label>Zip Code</label>  &nbsp; &nbsp; &nbsp; <input type="text" />
-        <p />
-        <button type='Submit'>Make a donation!</button>
-      </form>
+    <div style={header}>
+      <div style={bigTextFlex}>
+        <h1 style={bigText}>You are making the world better.</h1>
+      </div>
+      <div style={donationText}>
+        <h1 style={step}>STEP 1</h1>
+        <h1 style={enterDonation}>Enter donation amount</h1>
+      </div>
     </div>
   );
 
