8b1c7c365b5b53d08dd046e93067230c74f43187
bobcheese21 <rlin20@uw.edu>
2020-04-14 18:34:26 -1000

    dropdown

--- frontend/src/components/DonatePage.js
+++ frontend/src/components/DonatePage.js
@@ -4,6 +4,9 @@ import { PrimaryButton, SecondaryButton, TertiartyButton } from '../shared/Butto
 import { ButtonToolbar, Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import CurrencyInput from 'react-currency-input';
 import CreditCardInput from 'react-credit-card-input';
+import SelectSearch from 'react-select-search';
+import '../style.css';
+
 
 function DonatePage() {
 
@@ -134,6 +137,21 @@ function DonatePage() {
       message: "Value cannot be blank" 
     }
   }
+
+  function handleChangeCountry(value) {
+    setFormValues(prevState => ({
+      ...prevState,
+      country: value
+    }))
+  }
+  function handleChangeState(value) {
+    setFormValues(prevState => ({
+      ...prevState,
+      state: value
+    }))
+  }
+
+
 
   const donateHeader = {
     display: "flex",
@@ -372,6 +390,20 @@ function DonatePage() {
     width: "50%",
   }
 
+  const dropdown = {
+    display: "block",
+    width: "100%",
+    height: "50px",
+    border: "none",
+    background: "none",
+    outline: "none",
+    fontSize: "16px",
+    padding:" 0 20px",
+    color: "#222f3e",
+    boxSizing: "border-box",
+    position: "relative"
+  }
+
   // this is step one of the donation process
   if ((!nextToPaymentPressed || donateAmount == 0)) {
     return (<div style={donateHeader}>
@@ -402,12 +434,17 @@ function DonatePage() {
           }
         >
           <h1 style={whyTheseAmountsText}>Why these amounts?</h1>
+          
         </OverlayTrigger>
+
         {/*<h1 style={supportSiteText}>Would you like to help support this site?</h1>
         <div style={supportForm}>
           <CurrencyInput onChangeEvent={handleSupportUsAmountChange} style={supportInput} prefix="$" value={supportUsAmount} />
         </div>*/}
         <PrimaryButton onClick={() => handleNextClick(true)} text="Next: Payment information" />
+        <br></br>
+        <a style={support} href="https://www.patreon.com/care37">Click here to support us!</a>
+
       </div>
     </div>
     )
@@ -448,12 +485,28 @@ function DonatePage() {
             </div>
             <input style={paymentFieldInput} type="text" onChange={(e) => handleChange(e, [noBlanks])} placeholder="Email address" name={"email"} value={email}></input>
             <h1 style={paymentInfoSections}>Address</h1>
-            <input style={paymentFieldInput} type="text" onChange={(e) => handleChange(e, [noBlanks])} placeholder="Country" name={"country"}  value={country}></input>
+            <SelectSearch
+              key="countries"
+              value={country}
+              name = {"country"}
+              options={countries}
+              onChange={handleChangeCountry}
+              placeholder="Your country"
+              search
+            />
             <input style={paymentFieldInput} type="text" onChange={(e) => handleChange(e, [noBlanks])} placeholder="Address 1" name={"address1"} value={address1}></input>
             <input style={paymentFieldInput} type="text" onChange={(e) => handleChange(e, [noBlanks])} placeholder="Address 2" name={"address2"}  value={address2}></input>
             <div style={nameContainer}>
-              <input style={paymentFieldInput} type="text" onChange={(e) => handleChange(e, [noBlanks])} placeholder="City" name={"city"} value={city}></input>
-              <input style={paymentFieldInput} type="text" onChange={(e) => handleChange(e, [noBlanks])} placeholder="State" name={"state"}  value={state}></input>
+            <input style={paymentFieldInput} type="text" onChange={(e) => handleChange(e, [noBlanks])} placeholder="City" name={"city"} value={city}></input>
+            <SelectSearch
+              key="states"
+              value={state}
+              name={"state"}
+              options={states}
+              onChange={handleChangeState}
+              placeholder="Your state"
+              search
+            />
             </div>
           </div>
           <h1 style={paymentInfoSections}>Payment information</h1>
@@ -478,3 +531,317 @@ function DonatePage() {
 }
 
 export default DonatePage;
+
+
+
+
+
+const countries = [
+  { name: "Andorra", value: "Andorra" },
+  { name: "United Arab Emirates", value: "United Arab Emirates" },
+  { name: "Afghanistan", value: "Afghanistan" },
+  { name: "Antigua and Barbuda", value: "Antigua and Barbuda" },
+  { name: "Anguilla", value: "Anguilla" },
+  { name: "Albania", value: "Albania" },
+  { name: "Armenia", value: "Armenia" },
+  { name: "Angola", value: "Angola" },
+  { name: "Antarctica", value: "Antarctica" },
+  { name: "Argentina", value: "Argentina" },
+  { name: "American Samoa", value: "American Samoa" },
+  { name: "Austria", value: "Austria" },
+  { name: "Australia", value: "Australia" },
+  { name: "Aruba", value: "Aruba" },
+  { name: "Åland Islands", value: "Åland Islands" },
+  { name: "Azerbaijan", value: "Azerbaijan" },
+  { name: "Bosnia and Herzegovina", value: "Bosnia and Herzegovina" },
+  { name: "Barbados", value: "Barbados" },
+  { name: "Bangladesh", value: "Bangladesh" },
+  { name: "Belgium", value: "Belgium" },
+  { name: "Burkina Faso", value: "Burkina Faso" },
+  { name: "Bulgaria", value: "Bulgaria" },
+  { name: "Bahrain", value: "Bahrain" },
+  { name: "Burundi", value: "Burundi" },
+  { name: "Benin", value: "Benin" },
+  { name: "Saint Barthélemy", value: "Saint Barthélemy" },
+  { name: "Bermuda", value: "Bermuda" },
+  { name: "Brunei Darussalam", value: "Brunei Darussalam" },
+  { name: "Bolivia, Plurinational State of", value: "Bolivia, Plurinational State of" },
+  { name: "Bonaire, Sint Eustatius and Saba", value: "Bonaire, Sint Eustatius and Saba" },
+  { name: "Brazil", value: "Brazil" },
+  { name: "Bahamas", value: "Bahamas" },
+  { name: "Bhutan", value: "Bhutan" },
+  { name: "Bouvet Island", value: "Bouvet Island" },
+  { name: "Botswana", value: "Botswana" },
+  { name: "Belarus", value: "Belarus" },
+  { name: "Belize", value: "Belize" },
+  { name: "Canada", value: "Canada" },
+  { name: "Cocos (Keeling) Islands", value: "Cocos (Keeling) Islands" },
+  { name: "Congo, Democratic Republic of the", value: "Congo, Democratic Republic of the" },
+  { name: "Central African Republic", value: "Central African Republic" },
+  { name: "Congo", value: "Congo" },
+  { name: "Switzerland", value: "Switzerland" },
+  { name: "Côte d'Ivoire", value: "Côte d'Ivoire" },
+  { name: "Cook Islands", value: "Cook Islands" },
+  { name: "Chile", value: "Chile" },
+  { name: "Cameroon", value: "Cameroon" },
+  { name: "China", value: "China" },
+  { name: "Colombia", value: "Colombia" },
+  { name: "Costa Rica", value: "Costa Rica" },
+  { name: "Cuba", value: "Cuba" },
+  { name: "Cabo Verde", value: "Cabo Verde" },
+  { name: "Curaçao", value: "Curaçao" },
+  { name: "Christmas Island", value: "Christmas Island" },
+  { name: "Cyprus", value: "Cyprus" },
+  { name: "Czechia", value: "Czechia" },
+  { name: "Germany", value: "Germany" },
+  { name: "Djibouti", value: "Djibouti" },
+  { name: "Denmark", value: "Denmark" },
+  { name: "Dominica", value: "Dominica" },
+  { name: "Dominican Republic", value: "Dominican Republic" },
+  { name: "Algeria", value: "Algeria" },
+  { name: "Ecuador", value: "Ecuador" },
+  { name: "Estonia", value: "Estonia" },
+  { name: "Egypt", value: "Egypt" },
+  { name: "Western Sahara", value: "Western Sahara" },
+  { name: "Eritrea", value: "Eritrea" },
+  { name: "Spain", value: "Spain" },
+  { name: "Ethiopia", value: "Ethiopia" },
+  { name: "Finland", value: "Finland" },
+  { name: "Fiji", value: "Fiji" },
+  { name: "Falkland Islands (Malvinas)", value: "Falkland Islands (Malvinas)" },
+  { name: "Micronesia, Federated States of", value: "Micronesia, Federated States of" },
+  { name: "Faroe Islands", value: "Faroe Islands" },
+  { name: "France", value: "France" },
+  { name: "Gabon", value: "Gabon" },
+  { name: "United Kingdom of Great Britain and Northern Ireland", value: "United Kingdom of Great Britain and Northern Ireland" },
+  { name: "Grenada", value: "Grenada" },
+  { name: "Georgia", value: "Georgia" },
+  { name: "French Guiana", value: "French Guiana" },
+  { name: "Guernsey", value: "Guernsey" },
+  { name: "Ghana", value: "Ghana" },
+  { name: "Gibraltar", value: "Gibraltar" },
+  { name: "Greenland", value: "Greenland" },
+  { name: "Gambia", value: "Gambia" },
+  { name: "Guinea", value: "Guinea" },
+  { name: "Guadeloupe", value: "Guadeloupe" },
+  { name: "Equatorial Guinea", value: "Equatorial Guinea" },
+  { name: "Greece", value: "Greece" },
+  { name: "South Georgia and the South Sandwich Islands", value: "South Georgia and the South Sandwich Islands" },
+  { name: "Guatemala", value: "Guatemala" },
+  { name: "Guam", value: "Guam" },
+  { name: "Guinea-Bissau", value: "Guinea-Bissau" },
+  { name: "Guyana", value: "Guyana" },
+  { name: "Hong Kong", value: "Hong Kong" },
+  { name: "Heard Island and McDonald Islands", value: "Heard Island and McDonald Islands" },
+  { name: "Honduras", value: "Honduras" },
+  { name: "Croatia", value: "Croatia" },
+  { name: "Haiti", value: "Haiti" },
+  { name: "Hungary", value: "Hungary" },
+  { name: "Indonesia", value: "Indonesia" },
+  { name: "Ireland", value: "Ireland" },
+  { name: "Israel", value: "Israel" },
+  { name: "Isle of Man", value: "Isle of Man" },
+  { name: "India", value: "India" },
+  { name: "British Indian Ocean Territory", value: "British Indian Ocean Territory" },
+  { name: "Iraq", value: "Iraq" },
+  { name: "Iran, Islamic Republic of", value: "Iran, Islamic Republic of" },
+  { name: "Iceland", value: "Iceland" },
+  { name: "Italy", value: "Italy" },
+  { name: "Jersey", value: "Jersey" },
+  { name: "Jamaica", value: "Jamaica" },
+  { name: "Jordan", value: "Jordan" },
+  { name: "Japan", value: "Japan" },
+  { name: "Kenya", value: "Kenya" },
+  { name: "Kyrgyzstan", value: "Kyrgyzstan" },
+  { name: "Cambodia", value: "Cambodia" },
+  { name: "Kiribati", value: "Kiribati" },
+  { name: "Comoros", value: "Comoros" },
+  { name: "Saint Kitts and Nevis", value: "Saint Kitts and Nevis" },
+  { name: "Korea, Democratic People's Republic of", value: "Korea, Democratic People's Republic of" },
+  { name: "Korea, Republic of", value: "Korea, Republic of" },
+  { name: "Kuwait", value: "Kuwait" },
+  { name: "Cayman Islands", value: "Cayman Islands" },
+  { name: "Kazakhstan", value: "Kazakhstan" },
+  { name: "Lao People's Democratic Republic", value: "Lao People's Democratic Republic" },
+  { name: "Lebanon", value: "Lebanon" },
+  { name: "Saint Lucia", value: "Saint Lucia" },
+  { name: "Liechtenstein", value: "Liechtenstein" },
+  { name: "Sri Lanka", value: "Sri Lanka" },
+  { name: "Liberia", value: "Liberia" },
+  { name: "Lesotho", value: "Lesotho" },
+  { name: "Lithuania", value: "Lithuania" },
+  { name: "Luxembourg", value: "Luxembourg" },
+  { name: "Latvia", value: "Latvia" },
+  { name: "Libya", value: "Libya" },
+  { name: "Morocco", value: "Morocco" },
+  { name: "Monaco", value: "Monaco" },
+  { name: "Moldova, Republic of", value: "Moldova, Republic of" },
+  { name: "Montenegro", value: "Montenegro" },
+  { name: "Saint Martin, (French part)", value: "Saint Martin, (French part)" },
+  { name: "Madagascar", value: "Madagascar" },
+  { name: "Marshall Islands", value: "Marshall Islands" },
+  { name: "North Macedonia", value: "North Macedonia" },
+  { name: "Mali", value: "Mali" },
+  { name: "Myanmar", value: "Myanmar" },
+  { name: "Mongolia", value: "Mongolia" },
+  { name: "Macao", value: "Macao" },
+  { name: "Northern Mariana Islands", value: "Northern Mariana Islands" },
+  { name: "Martinique", value: "Martinique" },
+  { name: "Mauritania", value: "Mauritania" },
+  { name: "Montserrat", value: "Montserrat" },
+  { name: "Malta", value: "Malta" },
+  { name: "Mauritius", value: "Mauritius" },
+  { name: "Maldives", value: "Maldives" },
+  { name: "Malawi", value: "Malawi" },
+  { name: "Mexico", value: "Mexico" },
+  { name: "Malaysia", value: "Malaysia" },
+  { name: "Mozambique", value: "Mozambique" },
+  { name: "Namibia", value: "Namibia" },
+  { name: "New Caledonia", value: "New Caledonia" },
+  { name: "Niger", value: "Niger" },
+  { name: "Norfolk Island", value: "Norfolk Island" },
+  { name: "Nigeria", value: "Nigeria" },
+  { name: "Nicaragua", value: "Nicaragua" },
+  { name: "Netherlands", value: "Netherlands" },
+  { name: "Norway", value: "Norway" },
+  { name: "Nepal", value: "Nepal" },
+  { name: "Nauru", value: "Nauru" },
+  { name: "Niue", value: "Niue" },
+  { name: "New Zealand", value: "New Zealand" },
+  { name: "Oman", value: "Oman" },
+  { name: "Panama", value: "Panama" },
+  { name: "Peru", value: "Peru" },
+  { name: "French Polynesia", value: "French Polynesia" },
+  { name: "Papua New Guinea", value: "Papua New Guinea" },
+  { name: "Philippines", value: "Philippines" },
+  { name: "Pakistan", value: "Pakistan" },
+  { name: "Poland", value: "Poland" },
+  { name: "Saint Pierre and Miquelon", value: "Saint Pierre and Miquelon" },
+  { name: "Pitcairn", value: "Pitcairn" },
+  { name: "Puerto Rico", value: "Puerto Rico" },
+  { name: "Palestine, State of", value: "Palestine, State of" },
+  { name: "Portugal", value: "Portugal" },
+  { name: "Palau", value: "Palau" },
+  { name: "Paraguay", value: "Paraguay" },
+  { name: "Qatar", value: "Qatar" },
+  { name: "Réunion", value: "Réunion" },
+  { name: "Romania", value: "Romania" },
+  { name: "Serbia", value: "Serbia" },
+  { name: "Russian Federation", value: "Russian Federation" },
+  { name: "Rwanda", value: "Rwanda" },
+  { name: "Saudi Arabia", value: "Saudi Arabia" },
+  { name: "Solomon Islands", value: "Solomon Islands" },
+  { name: "Seychelles", value: "Seychelles" },
+  { name: "Sudan", value: "Sudan" },
+  { name: "Sweden", value: "Sweden" },
+  { name: "Singapore", value: "Singapore" },
+  { name: "Saint Helena, Ascension and Tristan da Cunha", value: "Saint Helena, Ascension and Tristan da Cunha" },
+  { name: "Slovenia", value: "Slovenia" },
+  { name: "Svalbard and Jan Mayen", value: "Svalbard and Jan Mayen" },
+  { name: "Slovakia", value: "Slovakia" },
+  { name: "Sierra Leone", value: "Sierra Leone" },
+  { name: "San Marino", value: "San Marino" },
+  { name: "Senegal", value: "Senegal" },
+  { name: "Somalia", value: "Somalia" },
+  { name: "South Sudan", value: "South Sudan" },
+  { name: "Sao Tome and Principe", value: "Sao Tome and Principe" },
+  { name: "El Salvador", value: "El Salvador" },
+  { name: "Sint Maarten, (Dutch part)", value: "Sint Maarten, (Dutch part)" },
+  { name: "Syrian Arab Republic", value: "Syrian Arab Republic" },
+  { name: "Eswatini", value: "Eswatini" },
+  { name: "Turks and Caicos Islands", value: "Turks and Caicos Islands" },
+  { name: "Chad", value: "Chad" },
+  { name: "French Southern Territories", value: "French Southern Territories" },
+  { name: "Togo", value: "Togo" },
+  { name: "Thailand", value: "Thailand" },
+  { name: "Tajikistan", value: "Tajikistan" },
+  { name: "Tokelau", value: "Tokelau" },
+  { name: "Timor-Leste", value: "Timor-Leste" },
+  { name: "Turkmenistan", value: "Turkmenistan" },
+  { name: "Tunisia", value: "Tunisia" },
+  { name: "Tonga", value: "Tonga" },
+  { name: "Turkey", value: "Turkey" },
+  { name: "Trinidad and Tobago", value: "Trinidad and Tobago" },
+  { name: "Tuvalu", value: "Tuvalu" },
+  { name: "Taiwan, Province of China", value: "Taiwan, Province of China" },
+  { name: "Tanzania, United Republic of", value: "Tanzania, United Republic of" },
+  { name: "Ukraine", value: "Ukraine" },
+  { name: "Uganda", value: "Uganda" },
+  { name: "United States Minor Outlying Islands", value: "United States Minor Outlying Islands" },
+  { name: "United States of America", value: "United States of America" },
+  { name: "Uruguay", value: "Uruguay" },
+  { name: "Uzbekistan", value: "Uzbekistan" },
+  { name: "Holy See", value: "Holy See" },
+  { name: "Saint Vincent and the Grenadines", value: "Saint Vincent and the Grenadines" },
+  { name: "Venezuela, Bolivarian Republic of", value: "Venezuela, Bolivarian Republic of" },
+  { name: "Virgin Islands, British", value: "Virgin Islands, British" },
+  { name: "Virgin Islands, U.S.", value: "Virgin Islands, U.S." },
+  { name: "Viet Nam", value: "Viet Nam" },
+  { name: "Vanuatu", value: "Vanuatu" },
+  { name: "Wallis and Futuna", value: "Wallis and Futuna" },
+  { name: "Samoa", value: "Samoa" },
+  { name: "Yemen", value: "Yemen" },
+  { name: "Mayotte", value: "Mayotte" },
+  { name: "South Africa", value: "South Africa" },
+  { name: "Zambia", value: "Zambia" }
+]
+
+const states = [
+  { name: "Alaska", value: "Alaska" },
+  { name: "Alabama", value: "Alabama" },
+  { name: "Arkansas", value: "Arkansas" },
+  { name: "American Samoa", value: "American Samoa" },
+  { name: "Arizona", value: "Arizona" },
+  { name: "California", value: "California" },
+  { name: "Colorado", value: "Colorado" },
+  { name: "Connecticut", value: "Connecticut" },
+  { name: "District of Columbia", value: "District of Columbia" },
+  { name: "Delaware", value: "Delaware" },
+  { name: "Florida", value: "Florida" },
+  { name: "Georgia", value: "Georgia" },
+  { name: "Guam", value: "Guam" },
+  { name: "Hawaii", value: "Hawaii" },
+  { name: "Iowa", value: "Iowa" },
+  { name: "Idaho", value: "Idaho" },
+  { name: "Illinois", value: "Illinois" },
+  { name: "Indiana", value: "Indiana" },
+  { name: "Kansas", value: "Kansas" },
+  { name: "Kentucky", value: "Kentucky" },
+  { name: "Louisiana", value: "Louisiana" },
+  { name: "Massachusetts", value: "Massachusetts" },
+  { name: "Maryland", value: "Maryland" },
+  { name: "Maine", value: "Maine" },
+  { name: "Michigan", value: "Michigan" },
+  { name: "Minnesota", value: "Minnesota" },
+  { name: "Missouri", value: "Missouri" },
+  { name: "Northern Mariana Islands", value: "Northern Mariana Islands" },
+  { name: "Mississippi", value: "Mississippi" },
+  { name: "Montana", value: "Montana" },
+  { name: "North Carolina", value: "North Carolina" },
+  { name: "North Dakota", value: "North Dakota" },
+  { name: "Nebraska", value: "Nebraska" },
+  { name: "New Hampshire", value: "New Hampshire" },
+  { name: "New Jersey", value: "New Jersey" },
+  { name: "New Mexico", value: "New Mexico" },
+  { name: "Nevada", value: "Nevada" },
+  { name: "New York", value: "New York" },
+  { name: "Ohio", value: "Ohio" },
+  { name: "Oklahoma", value: "Oklahoma" },
+  { name: "Oregon", value: "Oregon" },
+  { name: "Pennsylvania", value: "Pennsylvania" },
+  { name: "Puerto Rico", value: "Puerto Rico" },
+  { name: "Rhode Island", value: "Rhode Island" },
+  { name: "South Carolina", value: "South Carolina" },
+  { name: "South Dakota", value: "South Dakota" },
+  { name: "Tennessee", value: "Tennessee" },
+  { name: "Texas", value: "Texas" },
+  { name: "United States Minor Outlying Islands", value: "United States Minor Outlying Islands" },
+  { name: "Utah", value: "Utah" },
+  { name: "Virginia", value: "Virginia" },
+  { name: "Virgin Islands U.S.", value: "Virgin Islands U.S." },
+  { name: "Vermont", value: "Vermont" },
+  { name: "Washington", value: "Washington" },
+  { name: "Wisconsin", value: "Wisconsin" },
+  { name: "West Virginia", value: "West Virginia" },
+  { name: "Wyoming", value: "Wyoming" }
+]
