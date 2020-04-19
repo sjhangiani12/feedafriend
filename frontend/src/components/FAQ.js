6871e88af79932f0b9eba76a5c0582a98589003a
bobcheese21 <rlin20@uw.edu>
2020-04-19 13:45:58 -1000

    FAQ sizing' '

--- frontend/src/components/FAQ.js
+++ frontend/src/components/FAQ.js
@@ -75,11 +75,11 @@ class Q extends React.Component {
       marginRight: "1%",
     }
     return (
-      <div style={{ display: "flex", align: "center", alignItems: "center" }}>
+      <div style={{ display: "flex", align: "center", alignItems: "center", width: "100%" }}>
         <div style={line} >
-          <div style={{ display: "flex", alignItems: "center"}}>
+          <div style={{ display: "flex", alignItems: "center", width: "100%"}}>
             <span style={{ fontSize: "4em", marginLeft: "1%", color: "#1136FC", fontStyle: "bold", marginLeft: "5px", marginRight: "10px"}}>Q.  </span>
-            <h3 align="left" style={{ whiteSpace: "nowrap", marginTop: "5%"}}>{this.props.children}</h3>
+            <span id="Qqs" align="left" style={{fontSize: "2.2vmax", width: "100%", marginTop: "5%"}}>{this.props.children}</span>
           </div>
         </div>
       </div>
@@ -105,7 +105,7 @@ class A extends React.Component {
         <div style={line} >
           <div style={{ display: "flex", alignItems: "center" }}>
             <span style={{ fontSize: "4em", marginLeft: "1%", color: "#919191", fontStyle: "bold", marginLeft: "5px", marginRight: "10px" }}>A.  </span>
-            <span style={{ fontSize: "1.7em", color: "#919191"}} align="left">{this.props.children}</span>
+            <span style={{ fontSize: "2vmax", color: "#919191"}} align="left">{this.props.children}</span>
           </div>
         </div>
       </div>
@@ -117,83 +117,90 @@ function FAQ () {
 
 
   return (
-    <div className="container-fluid" style={{marginTop: "120px", marginBottom: "10% "}}>
-
-      < div className="row mx-5" style={{ alignItems: "center", justifyContent: "center"}} >
-        <div style={{marginBottom: "5%"}}>
+  
+    <div style={{ marginTop: "120px"}}>
+      < div className="row mx-5" style={{ alignItems: "center", justifyContent: "center" }} >
+        <div style={{ marginBottom: "5%" }}>
           <h1>Frequently Asked Questions (FAQ)</h1>
         </div>
-        
-        <div className="col-12" style={{
-          display: "flex", alignItems: "baseline", padding: "0"
-        }}>
-          <Q> Is this legit?</Q>
+      </ div>
+      <div className="container-fluid" style={{  marginBottom: "10% " }}>
+
+        < div className="row mx-5" style={{ alignItems: "center", justifyContent: "center" }} >
+
+          <div className="col-12" style={{
+            display: "flex", alignItems: "baseline", padding: "0"
+          }}>
+            <Q> Is this legit?</Q>
+          </div>
+          <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
+            <A> Yes! 100% of your donated funds become DoorDash gift cards for someone in need (we verify their situation by speaking to each and every person who signs up!)</A>
+          </div>
         </div>
-        <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
-          <A> Yes! 100% of your donated funds become DoorDash gift cards for someone in need (we verify their situation by speaking to each and every person who signs up!)</A>
+        <br></br>
+        <br></br>
+        <div className="row mx-5"  >
+          <div className="col-12" style={{
+            display: "flex", alignItems: "baseline", padding: "0"
+          }}>
+            <Q> How can I trust my data is secure?</Q>
+          </div>
+          <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
+            <A> We use SSL and HTTPS for end-to-end encryption when handling your credit card information. We port your information directly into DoorDash so that we never have to store any of your data.</A>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div className="row mx-5"  >
+          <div className="col-12" style={{
+            display: "flex", alignItems: "baseline", padding: "0"
+          }}>
+            <Q> How can I benefit?</Q>
+          </div>
+          <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
+            <A> If you are under significant monetary stress due to the COVID-19 health crisis, you can sign up for care37 <Link to="/receive">here</Link>. We will contact you and verify your status, at which point you will be placed in a queue to receive donated DoorDash credits.</A>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div className="row mx-5"  >
+          <div className="col-12" style={{
+            display: "flex", alignItems: "baseline", padding: "0"
+          }}>
+            <Q> Who are the recipients?</Q>
+          </div>
+          <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
+            <A> Every person who signs up to be a care37 recipient gets screened. We call each and every person to make sure they are in need of your donations. 100% of donated funds becomes a DoorDash gift card so they can enjoy a meal in the safety of their own home.</A>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div className="row mx-5"  >
+          <div className="col-12" style={{
+            display: "flex", alignItems: "baseline", padding: "0"
+          }}>
+            <Q> How do you choose who gets the funds?</Q>
+          </div>
+          <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
+            <A> Our database orders recipients by how recently they've been donated to and how much money they've been donated. The recipient who has received the least gets the next donation that comes in. </A>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div className="row mx-5"  >
+          <div className="col-12" style={{
+            display: "flex", alignItems: "baseline", padding: "0"
+          }}>
+            <Q> Are donations tax deductible?</Q>
+          </div>
+          <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
+            <A> We are currently working to attain our 501c(3) non-profit status. That said, we do not want to take on any additional liability - therefore we do not recommend claiming your donation as a tax deduction.</A>
+          </div>
         </div>
       </div>
-      <br></br>
-      <br></br>
-      <div className="row mx-5"  >
-        <div className="col-12" style={{
-          display: "flex", alignItems: "baseline", padding: "0"
-        }}>
-          <Q> How can I trust my data is secure?</Q>
-        </div>
-        <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
-          <A> We use SSL and HTTPS for end-to-end encryption when handling your credit card information. We port your information directly into DoorDash so that we never have to store any of your data.</A>
-        </div>
-      </div>
-      <br></br>
-      <br></br>
-      <div className="row mx-5"  >
-        <div className="col-12" style={{
-          display: "flex", alignItems: "baseline", padding: "0"}}>
-        <Q> How can I benefit?</Q>
-        </div>
-        <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0"}}>
-          <A> If you are under significant monetary stress due to the COVID-19 health crisis, you can sign up for care37 <Link to="/receive">here</Link>. We will contact you and verify your status, at which point you will be placed in a queue to receive donated DoorDash credits.</A>
-        </div>
-      </div>
-      <br></br>
-      <br></br>
-      <div className="row mx-5"  >
-        <div className="col-12" style={{
-          display: "flex", alignItems: "baseline", padding: "0"
-        }}>
-          <Q> Who are the recipients?</Q>
-        </div>
-        <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
-          <A> Every person who signs up to be a care37 recipient gets screened. We call each and every person to make sure they are in need of your donations. 100% of donated funds becomes a DoorDash gift card so they can enjoy a meal in the safety of their own home.</A>
-        </div>
-      </div>
-      <br></br>
-      <br></br>
-      <div className="row mx-5"  >
-        <div className="col-12" style={{
-          display: "flex", alignItems: "baseline", padding: "0"
-        }}>
-          <Q> How do you choose who gets the funds?</Q>
-        </div>
-        <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
-          <A> Our database orders recipients by how recently they've been donated to and how much money they've been donated. The recipient who has received the least gets the next donation that comes in. </A>
-        </div>
-      </div>
-      <br></br>
-      <br></br>
-      <div className="row mx-5"  >
-        <div className="col-12" style={{
-          display: "flex", alignItems: "baseline", padding: "0"
-        }}>
-          <Q> Are donations tax deductible?</Q>
-        </div>
-        <div className="col-12" style={{ display: "flex", alignItems: "baseline", padding: "0" }}>
-          <A> We are currently working to attain our 501c(3) non-profit status. That said, we do not want to take on any additional liability - therefore we do not recommend claiming your donation as a tax deduction.</A>
-        </div>
-      </div>
-    </div>
-    
+
+  </div>
+
   );
 }
 export default FAQ;
