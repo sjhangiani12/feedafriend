4791631b8259890f10e4f3f96d5e8118c7bac2c0
bobcheese21 <bobcheese21@gmail.com>
2020-05-03 17:19:22 -1000

    added forms and state logic

---
+++ frontend/src/components/RecipientInfo/Landing.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+function Landing(props) {
+
+  return (
+    <div>
+      <h1>
+        okay heres what is happening!
+      </h1>
+    </div>
+  )
+}
+
+
+export default Landing;
--- frontend/src/components/RecipientInfo/RecipientForm.js
+++ frontend/src/components/RecipientInfo/RecipientForm.js
@@ -1,15 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import UploadForm from './UploadForm';
+import UploadForm from './UploadForm.js';
+import Landing from './Landing.js';
+import { PrimaryButton, SecondaryButton } from "../../shared/ButtonComponents";
 
 function RecipientForm(props) {
 
-    const [step, setStep] = useState(1);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [bio, setBio] = useState("");
-    const [socialMediaLinks, setSocialMediaLinks] = useState([]);
-    const [uploads, setUploads] = useState([]);
-    const [profPic, setProfPic] = useState("");
+    const [step, setStep] = useState(0);
+    const [completed, setCompleted] = useState(false);
+
+    // const [firstName, setFirstName] = useState("");
+    // const [lastName, setLastName] = useState("");
+    // const [bio, setBio] = useState("");
+    // const [socialMediaLinks, setSocialMediaLinks] = useState([]);
+    // const [uploads, setUploads] = useState([]);
+    // const [profPic, setProfPic] = useState("");
+    // const [email, setEmail] = useState("");
+
+    const formDefaultValues = {
+        firstName: "",
+        lastName: "",
+        bio: "",
+        fb: "",
+        insta: "",
+        twit: "",
+        uploads: "",
+        profPic: "",
+        email: "",
+
+    }
+    const [formValues, setFormValues] = useState(formDefaultValues);
+    const { firstName, lastName, bio, uploads, profPic, email, fb, insta, twit} = formValues
+
+    function checkAllFieldsFilled() {
+        if (formValues.firstName != "" &&
+            formValues.lastName != "" &&
+            formValues.email != "" &&
+            formValues.bio != ""
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     function createProfile() {
         const data = {
@@ -18,7 +50,10 @@ function RecipientForm(props) {
             last_name: lastName,
             zip_code: 12345,
             bio: bio,
-            social_media_links: socialMediaLinks,
+            fb: fb,
+            insta: insta,
+            twit: twit,
+            email: email,
             prof_pic: profPic, 
             uploads: uploads 
         }
@@ -45,11 +80,116 @@ function RecipientForm(props) {
         )
     }
 
+    function handleNext() {
+        setStep(step+1);
+    }
+    
+    function handleChange(e) {
+        const target = e.target
+        setFormValues(prevState => ({
+            ...prevState,
+            [target.name]: target.value
+        }))
+        console.log(formValues)
+    }
+
+    function handleFinish() {
+        if (checkAllFieldsFilled()) {
+            setCompleted(true);
+            console.log(formValues)
+        } else {
+            alert("Please fill out all the fields.");
+        }
+    }
+
+    const toRender = {
+        0: <Step0 />,
+        1: <Step1 handle={handleChange}/>,
+        2: <Step2 handle={handleChange}/>,
+        3: <Step3 handle={handleChange}/>,
+        4: <Step4 handle={handleChange}/>,
+        5: <Step5 handle={handleChange}/>,
+        6: <Done onClick={handleFinish}/>
+    }
     return (
+        <>
+            {step < 6 ?     <PrimaryButton onClick={() => handleNext()} text="Next"></PrimaryButton> : null }
         <div>
-            <UploadForm />            
+            {toRender[step]}
         </div>
+        </>
+
     );
+
 }
 
+function Step0(props) {
+    return (
+        <>
+            <h1>Welcome to your fucking doom</h1>
+        </>
+    )
+}
+
+function Step1(props) {
+    return(
+        <>
+        <h1>Hi. What's your name?</h1>
+            <input placeholder="First Name" name="firstName" onChange={(e) => props.handle(e)}></input>
+            <input placeholder="Last Name" name="lastName" onChange={(e) => props.handle(e)}></input>
+
+        </>
+    )
+}
+
+function Step2(props) {
+    return (
+        <>
+            <h1>Tell us what you're going through.</h1>
+            <input placeholder="Your Story" name="bio" onChange={(e) => props.handle(e)}></input>
+
+        </>
+    )
+}
+
+function Step3(props) {
+    return (
+        <>
+            <h1>Where can people find you?</h1>
+            <input placeholder="@yourFB" name="fb" onChange={(e) => props.handle(e)}></input>
+            <input placeholder="@yourInstagram" name="insta" onChange={(e) => props.handle(e)}></input>
+            <input placeholder="@yourTwitter" name="twit" onChange={(e) => props.handle(e)}></input>
+
+        </>
+    )
+}
+
+function Step4(props) {
+    return (
+        <>
+            <h1>What is your email?</h1>
+            <input placeholder="ohHey@HowYou.doin" name="email" onChange={(e) => props.handle(e)}></input>
+
+        </>
+    )
+}
+
+
+function Step5(props) {
+    return (
+        <>
+            <h1>Step5</h1>
+        </>
+    )
+}
+
+
+function Done(props) {
+    return (
+        <>
+            <h1>Hell yeah bitch!</h1>
+            <PrimaryButton onClick={() => props.onClick()}></PrimaryButton>
+        </>
+    )
+}
 export default RecipientForm;
