17c1cab2cd3befa2b20ac02c89d99703f04dd773
bobcheese21 <bobcheese21@gmail.com>
2020-05-09 12:53:15 -1000

    added donor landing

--- frontend/src/components/DonatePage.js
+++ frontend/src/components/DonatePage.js
@@ -1,30 +1,44 @@
 import React, { useState } from 'react';
 import RecipientPicker from './RecipientPicker';
 import DonationFlow from './DonateFlow'
+import DonorLanding from './DonorLanding'
+import { PrimaryButton, SecondaryButton, TertiartyButton } from '../shared/ButtonComponents.js';
 
 function DonatePage() {
 
     const [recipientEmail, setRecipientEmail] = useState("");
     const [recipientFirstName, setRecipientFirstName] = useState("");
     const [recipientLastName, setRecipientLastName] = useState("");
+    const [finishedLanding, setFinishedLanding] = useState(false);
+
 
     return (
         <div>
-            { recipientEmail == "" && (
-                <RecipientPicker 
-                        setRecipientEmail={setRecipientEmail}
-                        setRecipientFirstName={setRecipientFirstName}
-                        setRecipientLastName={setRecipientLastName}
+            {!finishedLanding ? (
+                <>
+                    <DonorLanding letsGo={<PrimaryButton text="Let's get donating!" onClick={() => setFinishedLanding(true)}></PrimaryButton>}></DonorLanding>
+
+                </>
+            ) : (
+                <>
+                    {recipientEmail == "" && (
+                        <RecipientPicker
+                            setRecipientEmail={setRecipientEmail}
+                            setRecipientFirstName={setRecipientFirstName}
+                            setRecipientLastName={setRecipientLastName}
                         />
-            )}
-            { recipientEmail != "" && (
-                <DonationFlow 
-                        setRecipientEmail={setRecipientEmail}
-                        recipientEmail={recipientEmail}
-                        recipientFirstName={recipientFirstName}
-                        recipientLastName={recipientLastName}
+                    )}
+                    {recipientEmail != "" && (
+                        <DonationFlow
+                            setRecipientEmail={setRecipientEmail}
+                            recipientEmail={recipientEmail}
+                            recipientFirstName={recipientFirstName}
+                            recipientLastName={recipientLastName}
                         />
+                    )}
+                </>
             )}
+
         </div>
     );
 }
---
+++ frontend/src/components/DonorLanding.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import forDonor from "../static/toDonate.svg"
+
+function DonateLanding(props) {
+
+  return (
+    <>
+      <div style={root}>
+        <div className="container-fluid">
+          <div className="row flex-wrap" style={header}>
+            <div className="col-md-7 col-sm-12" style={{ marginTop: "5%", paddingRight: "2%" }} >
+              <h1 style={{ fontSize: "3em", margin: "0px" }}>Give the gift of a meal!</h1>
+
+              {/* <p style={{ color: "#828282", fontFamily: "sans-serif", fontWeight: "bold" }}>WHAT WE DO</p> */}
+
+              <h3 style={{ color: "#828282", fontFamily: "sans-serif", marginRight: "10%", marginTop: "5%" }}>You're about to see real people who need your help.</h3>
+              <div style={{ marginTop: "5%" }}>
+                {props.letsGo}
+              </div>
+              <img style={{ width: "50%", marginTop: "3%" }} src={forDonor} alt="header image" />
+
+
+            </div>
+            <div className="col-md-5 col-sm-12" style={{ marginTop: "5%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "left" }}>
+              <h2 style={{ marginBottom: "50px", paddingTop: "50px" }}>It's easy as 1, 2, 3</h2>
+              <div  style={{ display: "flex", flexDirection: "column", marginBottom: "5%" }}>
+                <div style={how2col}>
+                  <Q number="1">Select a Recipient.</Q>
+                </div>
+                <div style={how2col}>
+                  <Q number="2">Choose a donation amount.</Q>
+                </div>
+                <div style={how2col}>
+                  <Q number="3">Smile because you're helping!</Q>
+                </div>
+              </div>            
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+const root = {
+  position: "relative",
+  marginTop: "120px",
+  height: "100vh",
+  // left: "20%",
+  // width: "70%"
+  marginLeft: "40px",
+  marginRight: "40px",
+}
+const header = {
+  display: "flex",
+  flexWrap: "wrap",
+  marginLeft: "5%",
+  marginRight: "5%",
+}
+
+const how2col = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  marginBottom: "3%"
+
+}
+
+class Q extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false,
+    }
+  }
+
+
+  render() {
+    let line = {
+      borderLeft: "3px solid #1136FC",
+      height: "100%",
+      marginRight: "1%",
+    }
+    return (
+      <div style={{ display: "flex", align: "center", alignItems: "center", width: "100%" }}>
+        <div style={line} >
+          <div style={{ display: "flex", alignItems: "center", width: "100%" }}>
+            <span style={{ fontSize: "4em", marginLeft: "1%", color: "#1136FC", fontStyle: "bold", marginLeft: "5px", marginRight: "10px" }}>{this.props.number}. </span>
+            <span align="left" style={{ fontSize: "2vmax", width: "100%", marginTop: "5%" }}>{this.props.children}</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default DonateLanding;
